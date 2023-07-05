7447292cb436b776121e7e46692566f30bf49a6b
Yash Sharma <yash.sharma.now@gmail.com>
2023-07-05 13:24:33 -0700

    added header

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#site header
+st.title('Welcome to travlgo!')
+
+destination = st.text_input('Final Destination:', 'Paris')
+budget = st.number_input('Budget:', min_value=0, step=10)
+arrival_date = st.date_input("Arrival Date:")
+departure_date = st.date_input("Departure Date:")
